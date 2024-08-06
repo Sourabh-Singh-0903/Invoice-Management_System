f294eaeb735212a0ea63bb7f6def70f04741fcff
Sourabh-Singh-0903 <114512043+Sourabh-Singh-0903@users.noreply.github.com>
2024-08-07 00:42:49 +0530

    Delete src directory

--- src/main/java/com/luv2code/springboot/thymeleafdemo/dao/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springboot.thymeleafdemo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.luv2code.springboot.thymeleafdemo.entity.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
- 
-}
